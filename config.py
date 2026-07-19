56dd5c746612105deef2029845aa4c9e5a964c8e
NitbDheeraj <nitb.dheeraj@gmail.com>
2026-07-19 13:22:57 +0530

    Updating the version of psycopg2[binary]

--- config.py
+++ config.py
@@ -16,7 +16,9 @@ def _resolve_database_url():
     database_url = os.environ.get("DATABASE_URL")
     if database_url:
         if  database_url.startswith("postgres://"):
-            database_url = database_url.replace("postgres://", "postgresql://", 1)
+            database_url = database_url.replace("postgres://", "postgresql+psycopg://", 1)
+        elif database_url.startswith("postgresql://"):
+            database_url = database_url.replace("postgresql://", "postgresql+psycopg://", 1)
         return database_url
     return f"sqlite:///{os.path.join(DATABASE_DIR, 'learning_hub.db')}"
 
